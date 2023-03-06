3260cc6ce5500fd3be48abd6a551fd44b4ae8c57
Giansalvo Gusinu <profgusinu@gmail.com>
2023-03-06 14:31:50 +0100

    add connection to django server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var formdata = new FormData();
+       formdata.append("title", "from REACT");
+       formdata.append("user_id", "2345");
+    
+       var requestOptions = {
+         method: 'POST',
+         body: formdata,
+         redirect: 'follow',
+         changeOrigin: true,
+         mode: "cors",
+       };
+  
+    //Get Method
+    const doECHO = () => {
+      fetch("https://httpbin.org/post", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    };
+
+    //Get Method
+    const DoDebug = () => {
+      fetch("http://[::1]:8438/prediction/debug/", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    };
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <>Hello World by gians!</>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World by gians!</p>
+        <button onClick={doECHO}>ECHO from httpbin</button>
+        <button onClick={DoDebug}>Call DEBUG</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
