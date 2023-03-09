a3adb86701cddf7039b196c10fb79219387a1062
Giansalvo Gusinu <profgusinu@gmail.com>
2023-03-09 13:06:52 +0100

    add image preview and improved layout

--- src/App.js
+++ src/App.js
@@ -1,384 +1,159 @@
-import React, { useState, useCallback, useRef, useEffect, useMemo, forwardRef} from "react";
-import styled, { css } from "styled-components";
-import ReactCrop from "react-image-crop";
-import "react-image-crop/dist/ReactCrop.css";
-import Uploady, {
-  withRequestPreSendUpdate,
-  useItemFinalizeListener,
-  useItemStartListener,
-  useBatchAddListener,
-  useBatchFinishListener,
-  useUploady
-} from "@rpldy/uploady";
-import { getMockSenderEnhancer } from "@rpldy/mock-sender";
-import UploadButton from "@rpldy/upload-button";
-import UploadPreview, { PREVIEW_TYPES } from "@rpldy/upload-preview";
-import cropImage from "./cropImage";
-import { asUploadButton } from "@rpldy/upload-button";
+import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useRequestPreSend } from "@rpldy/uploady";
-import "./styles.css";
+import { useForm } from "react-hook-form";
+// import "./styles.css"
+import "./App.css"
+import logo_abinsula from './images/abinsula_logo.png';
+import logo_uniss from './images/uniss_logo.png';
+import logo_project from "./images/project_logo.svg"
+import image_placeholder from "./images/image_placeholder.png"
 
-function myEnhancer (data) {
-  console.log("Data:", data)
-  // console.log("Data.image[0]:", data.image[0])
+const imageMimeType = /image\/(png|jpg|jpeg)/i;
 
-  // const formData = new FormData()
-  // formData.append("image", data.image[0])
-  // formData.append("title", data.title);
-  // formData.append("description", data.description);
-  // formData.append("patient_id", data.patient_id);
-  // formData.append("patient_age", data.patient_age);
-  // formData.append("user_id", data.user_id);
+function App() {
 
-  // console.log("formData:", formData)
+    const [file, setFile] = useState(null);
+    const [fileDataURL, setFileDataURL] = useState(null);
 
-  // axios
-  // .post(
-  //     "http://[::1]:8438/prediction/do-prediction/",
-  //     formData,
-  //     {
-  //         headers: {
-  //             "Content-type": "multipart/form-data",
-  //         },
-  //     }
-  // )
-  // .then((res) => {
-  //     console.log(`Success` + res);
-  // })
-  // .catch((err) => {
-  //     console.log("Error: " + err);
-  // })
-};
+    const [fields, setFields] = useState({
+        image: null,
+        ground_truth_TODO: null,
+        title: "",
+        description: "",
+        patient_id: "",
+        patient_age: "",
+        user_id: 1});
 
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-const inputCss = css`
-  width: 260px;
-  height: 30px;
-  line-height: 30px;
-  font-size: 22px;
-  color: #000;
-  border: 1px solid #fff;
-  background-color: #f1f1f1;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-  padding: 0 4px;
-`;
+    const changeHandler = (e) => {
+        const file = e.target.files[0];
+        if (!file.type.match(imageMimeType)) {
+          alert("Image mime type is not valid");
+          return;
+        }
+        setFile(file);
+      }
+      useEffect(() => {
+        let fileReader, isCancel = false;
+        if (file) {
+          fileReader = new FileReader();
+          fileReader.onload = (e) => {
+            const { result } = e.target;
+            if (result && !isCancel) {
+              setFileDataURL(result)
+            }
+          }
+          fileReader.readAsDataURL(file);
+        }
+        return () => {
+          isCancel = true;
+          if (fileReader && fileReader.readyState === 1) {
+            fileReader.abort();
+          }
+        }
+    
+      }, [file]);
+    
 
-const StyledReactCrop = styled(ReactCrop)`
-  width: 100%;
-  max-width: 900px;
-  height: 400px;
-`;
+    function onSubmit (data) {
+        console.log("Data:", data)
+        console.log("Data.image[0]:", data.image[0])
 
-const PreviewImage = styled.img`
-  margin: 5px;
-  max-width: 200px;
-  height: auto;
-  max-height: 200px;
-`;
+        const formData = new FormData()
+        formData.append("image", data.image[0])
+        formData.append("title", data.title);
+        formData.append("description", data.description);
+        formData.append("patient_id", data.patient_id);
+        formData.append("patient_age", data.patient_age);
+        formData.append("user_id", data.user_id);
 
-const SubmitButton = styled.button`
-  height: 60px;
-  width: 260px;
-  font-size: 22px;
-  margin-top: 20px;
-  background-color: #101a2c;
-  border: 1px solid #4b5763;
-  color: #b0b1b3;
-  text-transform: uppercase;
-  cursor: pointer;
-  margin-bottom: 4px;
+        console.log("formData:", formData)
 
-  ${({ disabled }) =>
-    disabled
-      ? `
-    cursor: default;
-    background-color: #d8d2d2;
-    color: #444040
-  `
-      : ""}
-`;
-
-const ButtonsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 10px;
-`;
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  img {
-    margin-top: 10px;
-    max-width: 200px;
-    height: auto;
-    max-height: 200px;
-  }
-
-  input[type="text"],
-  input[type="number"] {
-    ${inputCss}
-  }
-`;
-
-const UploadField = styled.div`
-  ${inputCss}
-
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
-
-  cursor: pointer;
-`;
-
-
-const MyUploadField = asUploadButton(
-  forwardRef(({ onChange, ...props }, ref) => {
-    const [text, setText] = useState("Select file");
-
-    useBatchAddListener((batch) => {
-      setText(batch.items[0].file.name);
-      onChange(batch.items[0].file.name);
-    });
-
-    useBatchFinishListener(() => {
-      setText("Select file");
-      onChange(null);
-    });
+        axios
+        .post(
+            "http://[::1]:8438/prediction/do-prediction/",
+            formData,
+            {
+                headers: {
+                    "Content-type": "multipart/form-data",
+                },
+            }
+        )
+        .then((res) => {
+            console.log(`Success` + res);
+           
+        })
+        .catch((err) => {
+            console.log("Error: " + err);
+        })
+    };
 
     return (
-      <UploadField {...props} ref={ref} id="form-upload-button" title={text}>
-        {text}
-      </UploadField>
-    );
-  })
-);
+      <>
+        <header className="header">
+            <img src = {logo_project} height="75" alt="project logo"/>
+          Parkinson's Project
+        </header>
+        <div class="row">
+            <div class="column">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label for="image">Image: </label>
+            <input type="file" {...register("image", { required: true })} 
+                accept='.png, .jpg, .jpeg'
+                onChange={changeHandler} />
+            <br/>
+            <label for="title">Title: </label>
+            <br/>
+            <input {...register("title", { required: true })} />
+            <br/>
+            <label for="description">Description: </label>
+            <br/>
+            <input {...register("description", { required: true })} />
+            <br/>
+            <label for="user_id">User ID: </label>
+            <br/>
+            <input {...register("user_id", { required: true })} />
+            <br/>
+            <label for="patient_id">Patient ID: </label>
+            <br/>
+            <input {...register("patient_id", { required: true })} />
+            <br/>
+            <label for="patient_age">Patient age: </label>
+            <br/>
+            <input {...register("patient_age", { required: true })} />
+            <br/><br/>
 
-const PreviewButtons = ({
-  finished,
-  crop,
-  updateRequest,
-  onUploadCancel,
-  onUploadCrop
-}) => {
-  return (
-    <ButtonsWrapper>
-      <button
-        style={{
-          display: !finished && updateRequest && crop ? "block" : "none"
-        }}
-        onClick={onUploadCrop}
-      >
-        Upload Cropped
-      </button>
-      <button
-        style={{ display: !finished && updateRequest ? "block" : "none" }}
-        onClick={updateRequest}
-      >
-        Upload without Crop
-      </button>
-      <button
-        style={{
-          display: !finished && updateRequest && crop ? "block" : "none"
-        }}
-        onClick={onUploadCancel}
-      >
-        Cancel
-      </button>
-    </ButtonsWrapper>
-  );
-};
-
-const UPLOAD_STATES = {
-  NONE: 0,
-  UPLOADING: 1,
-  FINISHED: 2
-};
-
-const ItemPreviewWithCrop = withRequestPreSendUpdate((props) => {
-  const {
-    id,
-    url,
-    isFallback,
-    type,
-    updateRequest,
-    requestData,
-    previewMethods
-  } = props;
-  const cropRef = useRef(null);
-  const [uploadState, setUploadState] = useState(UPLOAD_STATES.NONE);
-  const [crop, setCrop] = useState(null);
-  const [croppedUrl, setCroppedUrl] = useState(null);
-  const isFinished = uploadState === UPLOAD_STATES.FINISHED;
-
-  // console.log(crop)
-  // console.log(croppedUrl)
-
-  useItemStartListener(() => setUploadState(UPLOAD_STATES.UPLOADING), id);
-  useItemFinalizeListener(() => setUploadState(UPLOAD_STATES.FINISHED), id);
-
-  const onImageLoaded = useCallback((image) => {
-    cropRef.current = image;
-  }, []);
-
-  const onUploadCrop = useCallback(async () => {
-    if (updateRequest && (crop?.height || crop?.width)) {
-      const { blob: croppedBlob, blobUrl, revokeUrl } = await cropImage(
-        cropRef.current,
-        requestData.items[0].file,
-        crop,
-        true
-      );
-
-      requestData.items[0].file = croppedBlob;
-
-      updateRequest({ items: requestData.items });
-      setCroppedUrl({ blobUrl, revokeUrl });
-      console.log(blobUrl)
-    }
-  }, [requestData, updateRequest, crop]);
-
-
-  const onUploadCancel = useCallback(() => {
-    updateRequest(false);
-    if (previewMethods.current?.clear) {
-      previewMethods.current.clear();
-    }
-  }, [updateRequest, previewMethods]);
-
-  useEffect(() => () => croppedUrl?.revokeUrl(), [croppedUrl]);
-
-  return isFallback || type !== PREVIEW_TYPES.IMAGE ? (
-    <PreviewImage src={url} alt="fallback img" />
-  ) : (
-    <>
-      {requestData && uploadState === UPLOAD_STATES.NONE ? (
-        <StyledReactCrop
-          ruleOfThirds
-          src={url}
-          crop={crop}
-          onImageLoaded={onImageLoaded}
-          onChange={setCrop}
-          onComplete={setCrop}
-          style={{ height: "100%" }}
-        />
-      ) : (
-        <PreviewImage src={croppedUrl?.blobUrl || url} alt="img to upload" />
-      )}
-      <PreviewButtons
-        finished={isFinished}
-        crop={crop}
-        updateRequest={updateRequest}
-        onUploadCancel={onUploadCancel}
-        onUploadCrop={onUploadCrop}
-      />
-      <p>{isFinished ? "FINISHED" : ""}</p>
+            {errors.title       && <span>This field is required</span>}
+            {errors.patient_id  && <span>This field is required</span>}        
+            {errors.user_id     && <span>This field is required</span>}        
+            
+            <input type="submit" />
+        </form>
+            </div>
+            <div class="column">
+                <h2>Column 2</h2>
+                <p>Some text..</p>
+                {fileDataURL ?
+                <p className="img-preview-wrapper">
+                {
+                    <img src={fileDataURL} alt="preview" />
+                }
+                </p> : <img src = {image_placeholder} width="100%" alt="placeholder"/>}
+            </div>
+            <div class="column">
+                <h2>Column 3</h2>
+                <p>Some text..</p>
+                <img src = {image_placeholder} width="100%" alt="placeholder"/>
+            </div>
+        </div>
+        <footer className="footer">
+            <img src={logo_uniss} height="75" alt="UNISS logo"/>
+            <img src={logo_abinsula} height="75" alt="Abinsula logo"/>
+        </footer> 
     </>
   );
-});
-
-
-const MyForm = () => {
-  const [fields, setFields] = useState({});
-  const [fileName, setFileName] = useState(null);
-  const { processPending } = useUploady();
-
-  const onSubmit = () => processPending({ params: fields });
-
-  const onFieldChange = (e) => {
-    setFields({
-      ...fields,
-      [e.currentTarget.id]: e.currentTarget.value
-    });
-  };
-
-  const buttonExtraProps = useMemo(
-    () => ({
-      onChange: setFileName
-    }),
-    [setFileName]
-  );
-
-  return (
-    <Form>
-      <MyUploadField autoUpload={false} extraProps={buttonExtraProps} />
-      <br />
-      <input
-        onChange={onFieldChange}
-        id="field-name"
-        type="text"
-        placeholder="your name"
-      />
-      <br />
-      <input
-        onChange={onFieldChange}
-        id="field-age"
-        type="number"
-        placeholder="your age"
-      />
-      <br />
-      <SubmitButton
-        id="form-submit"
-        type="button"
-        onClick={onSubmit}
-        disabled={!fileName}
-      >
-        Submit Form
-      </SubmitButton>
-    </Form>
-  );
-};
-
-//determine successful call using custom status codes
-const customIsSuccess = (xhr) => {console.log(xhr)};
-
-
-const MyComponent = () => {
-    useRequestPreSend(({ items, options }) => {
-
-      console.log("items: " + items)
-      console.log("options: " + options)
-      let method = options.method;
-
-        if (options.destination.url.startsWith("https://put-server")) {
-            method = "PUT";
-        }
-
-        return {
-            options: { method } //will be merged with the rest of the options
-        };
-    });
-
-};
-
-export default function App() {
-  const previewMethodsRef = useRef();
-
-  return (
-    <Uploady
-      multiple={false}
-      destination={{ url: "http://[::1]:8438/prediction/debug/" }}
-      enhancer={myEnhancer}
-      // params={"pippo"}
-      isSuccessfulCall={customIsSuccess}
-      method="POST"
-    >
-      <div className="App">
-        <h1>Hello React Uploady</h1>
-        <UploadButton>Select File to upload</UploadButton>
-        <br />
-        <UploadPreview
-          PreviewComponent={ItemPreviewWithCrop}
-          previewComponentProps={{ previewMethods: previewMethodsRef }}
-          previewMethodsRef={previewMethodsRef}
-          fallbackUrl="https://icon-library.net/images/image-placeholder-icon/image-placeholder-icon-6.jpg"
-        />
-      </div>
-      
-      <MyForm/>
-    </Uploady>
-  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <img
-      id="logo"
-      src="https://res.cloudinary.com/yoav-cloud/image/upload/v1587564735/rpldy/logo/react-uploady-text-logo-light.png"
-    />
     <App />
     <p
       style={{
@@ -20,18 +16,6 @@ ReactDOM.render(
         fontWeight: "bold"
       }}
     >
-      Check out my recent youtube tutorial with more advanced concepts and
-      examples:
-      <br />
-      <br />
-      <a
-        style={{ color: "#fff" }}
-        href="https://youtu.be/a6FHlPhw6Vg:"
-        target="_blank"
-        rel="noopener nofollow"
-      >
-        How to crop images before uploading in ReactJS
-      </a>
     </p>
   </React.StrictMode>,
   rootElement
