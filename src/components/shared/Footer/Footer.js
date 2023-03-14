1197732fa4543ef55cb168440a9ca880e7546254
Giansalvo Gusinu <profgusinu@gmail.com>
2023-03-14 14:14:33 +0100

    add PON logo to footer

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -4,6 +4,7 @@ import "./Footer.css"
 
 import logo_abinsula from '../../../images/abinsula_logo.png';
 import logo_uniss from '../../../images/uniss_logo.png';
+import logo_PON from '../../../images/PON_logo.png';
 
 export function Footer() {
 
@@ -13,6 +14,9 @@ export function Footer() {
             <footer class="footer_row">
                 <div class="footer_column">
                     <a href="https://www.uniss.it" target="_blank"><img src = {logo_uniss} height="50" alt="UNISS logo"/></a> 
+                </div>
+                <div class="footer_column">
+                    <img src = {logo_PON} height="50" alt="PON logo"/>
                 </div>
                 <div class="footer_column">
                     <a href="https://www.abinsula.com" target="_blank"><img src = {logo_abinsula} height="50" alt="Abinsula logo"/></a> 
