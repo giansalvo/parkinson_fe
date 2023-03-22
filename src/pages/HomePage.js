f98f7d72aa304f098e487a3fde1d782e070b7eb7
Giansalvo Gusinu <profgusinu@gmail.com>
2023-03-22 10:30:22 +0100

    added slideshow

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,20 +6,74 @@ import "./HomePage.css"
 import {Header} from "../components/shared/Header/Header";
 import { Footer } from "../components/shared/Footer/Footer";
 
+import img1 from "../images/img_ai1.jpg"
+import img2 from "../images/img_ai2.jpg"
+import img3 from "../images/img_ai3.jpg"
+import img4 from "../images/img_ai4.jpg"
+import img5 from "../images/img_ai5.jpg"
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "red", "pink"];
+const delay = 2500;
+const img = [img1, img2, img3, img4, img5]
+
 function HomePage() {
 
   console.log("HomePage")
 
-  const { handleSubmit, formState: { errors } } = useForm();
+  const {formState: { errors } } = useForm();
 
-  function onSubmit (data) {
-      console.log("onSubmit")
-      // push
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
     };
+  }, [index]);
 
     return (
-      <div class="main_container">
+      <div>
         <Header/>
+        <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((backgroundColor, index) => (
+          <div
+            className="slide"
+            key={index}
+          ><img src={img[index]}/></div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
         <div class="message">
           <h2>
             Welcome! This website uses a Neural Network Artificial Intelligence to identify and highlight the Substantia Nigra found in Parkinson's patients.
