a855042eefb487da344e8622d61bdc7e500cdd97
Giansalvo Gusinu <profgusinu@gmail.com>
2023-03-27 10:08:07 +0200

    ExportData is working

--- src/pages/ExportData.js
+++ src/pages/ExportData.js
@@ -1,41 +1,43 @@
 import React from "react";
-import { CSVLink } from "react-csv";
-
+import axios from 'axios'
+import {ExportToExcel} from './ExportToExcel'
 import "./HomePage.css"
 import {Header} from "../components/shared/Header/Header";
 import { Footer } from "../components/shared/Footer/Footer";
+
 
 function ExportData() {
 
   console.log("ExportData")
 
-  const headers = [
-    { label: "First Name", key: "firstname" },
-    { label: "Last Name", key: "lastname" },
-    { label: "Email", key: "email" }
-  ];
-  
-  const data = [
-    { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
-    { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
-    { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
-  ];
+  const [data, setData] = React.useState([])
+  const fileName = "dataset_sn_ai"; // here enter filename for your excel file
+
+  React.useEffect(() => {
+    const fetchData = () =>{
+    axios.get("http://[::1]:8438/prediction/do-dashboard/")
+    .then((res) => {
+        const obj = res.data.predictions;
+        setData(obj);
+        // alert("Data exported correctly.")
+     })
+     .catch((err) => {
+        alert("Error during request.")      
+        console.log("Error: " + err);
+    })
+    }
+    fetchData()
+  }, [])
 
   return (
-        <div class="main_container">
+        <div className="main_container">
             <Header/>
-            <div class="message">
+            <div className="message">
             Here you can download all data records. The download should start shortly and take just a few seconds. This data can be used only for research purposes.
             By clicking the following button you agree to follow our Terms & Conditions.
             <br/><br/>
             <center>
-            <CSVLink 
-                data={data} 
-                headers={headers}
-                className="button3"
-            >
-                Export Data
-            </CSVLink>
+            <ExportToExcel apiData={data} fileName={fileName}/>
             </center>
             </div>
             <Footer/>
